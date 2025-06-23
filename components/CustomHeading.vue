eae6d8caa885de32f16394147d6f92a4b8c605b3
COD3VIL <73862378+michaelnji@users.noreply.github.com>
2025-06-23 16:16:30 +0100

    Refactor CustomHeading styles for consistency and update blog layout for improved responsiveness and readability. Adjust image handling and loading states in blog post component.

--- components/CustomHeading.vue
+++ components/CustomHeading.vue
@@ -13,10 +13,10 @@ const style = computed(() => props.value.style)
         <h2 v-if="style === 'h2'" class="font-bold  text-4xl md:text-5xl">
             <slot></slot>
         </h2>
-        <h3 v-if="style === 'h3'" class="font-bold  text-3xl md:text-2xl">
+        <h3 v-if="style === 'h3'" class="!font-medium  text-3xl md:text-2xl">
             <slot></slot>
         </h3>
-        <h4 v-if="style === 'h4'" class=" font-bold  text-2xl md:text-3xl">
+        <h4 v-if="style === 'h4'" class=" !font-medium  text-2xl md:text-3xl">
             <slot></slot>
         </h4>
     </div>
