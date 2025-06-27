31c60643db986789f327afce85521a26c9d530b3
COD3VIL <73862378+michaelnji@users.noreply.github.com>
2025-06-27 18:28:29 +0100

    Refactor blog components to streamline data fetching and loading state management. Consolidate error handling with toast notifications and improve SEO metadata integration for better social media sharing. Clean up conditional rendering for enhanced code clarity.

--- pages/blog/[slug].vue
+++ pages/blog/[slug].vue
@@ -34,77 +34,76 @@ async function retry() {
 
 }
 onMounted(async () => {
-    if (!postsStore.posts && !postsStore.loading) {
-        try {
-            isLoading.value = true
-            await postsStore.fetchPosts()
-            console.log(selectedPost.value?.imgUrl)
 
-            isLoading.value = false
-        } catch (error) {
-            toast.error(generateHumanMessage(`${error}`), {
-                duration: 5000, action: {
-                    label: 'Retry',
-                    onClick: () => toast.promise(retry, {
-                        loading: 'Fetching...',
-                        success: 'Data has been loaded successfully',
-                        error: 'An error has occured, please reload this page'
+    try {
+        isLoading.value = true
+        await postsStore.fetchPosts()
+        isLoading.value = false
+        useHead({
+            htmlAttrs: { lang: 'en-US' }, // BCP 47 language code
+            link: [{
+                rel: 'canonical',
+                href: `${defaultSiteSettings.siteUrl}/blog/${selectedPost.value?.slug}`,
+                // content: `${defaultSiteSettings.siteUrl}/blog/${selectedPost.value?.slug}`
+            }]
+        })
+
+        useSeoMeta({
+            title: selectedPost.value?.title,
+            // titleTemplate: '%s',
+            description: selectedPost.value?.excerpt,
+            ogType: 'article',
+            articlePublishedTime: selectedPost.value?.publishedAt,
+            articleModifiedTime: selectedPost.value?._updatedAt,
+            // articleAuthor: selectedPost.value?.authorInfo.name,
+            // articleSection: 'Technology', // category
+            // articleTag: tags.value,
+            twitterLabel1: 'Author',
+            twitterData1: selectedPost.value?.authorInfo.name,
+            ogUrl: `${defaultSiteSettings.siteUrl}/blog/${selectedPost.value?.slug}`,
+            ogLocale: 'en_US',
+            ogSiteName: defaultSiteSettings.siteName,
+            twitterTitle: selectedPost.value?.title,
+            twitterDescription: selectedPost.value?.excerpt,
+
+            // no longer explicitly used by X but may be useful for SEO
+            // twitterSite: '@example',
+            // twitterCreator: '@example',
+
+            // og image
+            ogImage: {
+                url: `${selectedPost.value?.imgUrl}`,
+                width: 1400,
+                height: 750,
+                alt: selectedPost.value?.title,
+                type: 'image/png'
+            },
+            twitterImage: {
+                url: `${selectedPost.value?.imgUrl}`,
+                width: 1200,
+                height: 800,
+                alt: selectedPost.value?.title,
+                type: 'image/png'
+            },
+            // twitter image (note: ogImage is used as a fallback so this is optional)
+            twitterCard: 'summary_large_image', // or summary
+        })
+    } catch (error) {
+        toast.error(generateHumanMessage(`${error}`), {
+            duration: 5000, action: {
+                label: 'Retry',
+                onClick: () => toast.promise(retry, {
+                    loading: 'Fetching...',
+                    success: 'Data has been loaded successfully',
+                    error: 'An error has occured, please reload this page'
 
 
-                    })
-                },
-            })
-        }
+                })
+            },
+        })
     }
-    useHead({
-        htmlAttrs: { lang: 'en-US' }, // BCP 47 language code
-        link: [{
-            rel: 'canonical',
-            href: `${defaultSiteSettings.siteUrl}/blog/${selectedPost.value?.slug}`,
-            // content: `${defaultSiteSettings.siteUrl}/blog/${selectedPost.value?.slug}`
-        }]
-    })
 
-    useSeoMeta({
-        title: selectedPost.value?.title,
-        // titleTemplate: '%s',
-        description: selectedPost.value?.excerpt,
-        ogType: 'article',
-        articlePublishedTime: selectedPost.value?.publishedAt,
-        articleModifiedTime: selectedPost.value?._updatedAt,
-        // articleAuthor: selectedPost.value?.authorInfo.name,
-        // articleSection: 'Technology', // category
-        // articleTag: tags.value,
-        twitterLabel1: 'Author',
-        twitterData1: selectedPost.value?.authorInfo.name,
-        ogUrl: `${defaultSiteSettings.siteUrl}/blog/${selectedPost.value?.slug}`,
-        ogLocale: 'en_US',
-        ogSiteName: defaultSiteSettings.siteName,
-        twitterTitle: selectedPost.value?.title,
-        twitterDescription: selectedPost.value?.excerpt,
 
-        // no longer explicitly used by X but may be useful for SEO
-        // twitterSite: '@example',
-        // twitterCreator: '@example',
-
-        // og image
-        ogImage: {
-            url: `${selectedPost.value?.imgUrl}`,
-            width: 1400,
-            height: 750,
-            alt: selectedPost.value?.title,
-            type: 'image/png'
-        },
-        twitterImage: {
-            url: `${selectedPost.value?.imgUrl}`,
-            width: 1200,
-            height: 800,
-            alt: selectedPost.value?.title,
-            type: 'image/png'
-        },
-        // twitter image (note: ogImage is used as a fallback so this is optional)
-        twitterCard: 'summary_large_image', // or summary
-    })
 })
 
 
