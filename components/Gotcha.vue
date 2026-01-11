9abc3c8a79f8a11053c71999ba1b5a03192a9dea
theCodeD3vil <micheal.nji@newvcreatives.com>
2026-01-11 18:18:54 +0100

    Refactor font-weight classes in Gotcha, Hint, and Quote components for consistency

--- components/Gotcha.vue
+++ components/Gotcha.vue
@@ -8,9 +8,9 @@
             class="absolute size-16 grid place-items-center rounded-full border-6 border-base-300/30 -top-4 -left-4 !bg-warning">
             <Icon name="ph:warning-fill" size="30" class="!text-base-100" />
         </div>
-        <!-- <div class="font-bold mt-6 text-3xl">Hint</div> -->
+        <!-- <div class="font-bold mt-6 ">Hint</div> -->
         <div
-            class="bg-gradient-to-tl !p-0 !leading-normal !m-0 from-warning via-warning to-warning text-transparent bg-clip-text font-semibold ">
+            class="bg-gradient-to-tl !p-0 !leading-normal !m-0 from-warning via-warning to-warning text-transparent bg-clip-text font-normal-weight ">
             <slot />
         </div>
 
